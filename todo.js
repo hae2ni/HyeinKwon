5a8b0f5e5375a8649b7259e55e4749496a5889d3
HAE2NI <haein990222@naver.com>
2023-04-17 15:10:02 +0900

    📝  todo.js 추가

---
+++ todo.js
@@ -0,0 +1,18 @@
+const toDos = [
+  {
+    category: "할 일!",
+    todos: ["피아노연습", "운동가기"],
+  },
+  {
+    category: "SOPT",
+    todos: ["웹팟 세미나", "웹팟 뒤풀이"],
+  },
+  {
+    category: "영어 회화",
+    todos: ["전화 영어", "일기 쓰기"],
+  },
+  {
+    category: "공부",
+    todos: ["알고리즘", "인강듣기"],
+  },
+];
