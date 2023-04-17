e0177094c9823b6783b52402031fe26fe9f2a3cc
HAE2NI <haein990222@naver.com>
2023-04-17 15:11:35 +0900

    💄 js data 보이도록 ui 수정

--- todo.js
+++ todo.js
@@ -16,3 +16,24 @@ const toDos = [
     todos: ["알고리즘", "인강듣기"],
   },
 ];
+
+for (i = 0; i < toDos.length; i++) {
+  const section = document.querySelector(".whattodo");
+
+  const article = document.createElement("article");
+  article.className = "todo_wrapper";
+
+  const div = document.createElement("div");
+  div.className = "list";
+
+  section.appendChild(article);
+  article.appendChild(div);
+  div.innerText = toDos[i].category;
+
+  for (j = 0; j < toDos[i].todos.length; j++) {
+    const span = document.createElement("span");
+    article.appendChild(span);
+
+    span.innerText = toDos[i].todos[j];
+  }
+}
