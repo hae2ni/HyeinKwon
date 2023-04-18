80ebde83cc7f4fd3b38e677ca2355b9859e09c33
HAE2NI <haein990222@naver.com>
2023-04-18 17:13:55 +0900

    🎨 modal 띄워서 추가 기능 구현

--- data.js
+++ data.js
@@ -23,16 +23,8 @@ const toDos = [
   },
 ];
 
-const style = [
-  "#c7f9cc",
-  "#80ed99",
-  "#fefee3",
-  "#f4f0bb",
-  "#fbfaff",
-  "#ebd9fc",
-];
-
 const DONE_CLASSNAME = "done";
+let clickCategoryId;
 
 function initToDO() {
   const section = document.querySelector(".whattodo");
@@ -43,20 +35,29 @@ function initToDO() {
     article.className = "todo_wrapper";
     section.appendChild(article);
     const div = document.createElement("div");
+    div.id = data.category;
     div.className = "list";
     section.appendChild(article);
     article.appendChild(div);
     div.innerText = data.category;
-    div.addEventListener("click", addToDo);
+
+    div.addEventListener("click", function (event) {
+      clickCategoryId = event.target.id;
+      // console.log(evednt.target.id);
+      openModal();
+      // const clickCategory = d
+    });
 
     // 할 일 리스트 보이기
     data.todos.forEach((data) => {
       const span = document.createElement("span");
       article.appendChild(span);
       span.innerHTML = `
-    ${data.todo}`;
+    ${data.todo}
+    `;
       span.classList.add("not_done");
       innerHeart();
+
       span.addEventListener("click", function () {
         span.classList.toggle(DONE_CLASSNAME);
         notDoneNum();
@@ -64,7 +65,7 @@ function initToDO() {
     });
   });
 }
-
+initToDO();
 //미완료 할 일 갯수 세는 함수
 function notDoneNum() {
   const leftTodo = document.getElementsByClassName("not_done"); //안 끝낸 것
@@ -88,7 +89,6 @@ function innerHeart() {
   let leftTodoList = Array.from(leftTodo);
   const heart = document.querySelector(".todoing");
 
-  console.log(leftTodoList);
   heart.innerText = leftTodoList.length;
 }
 
@@ -96,13 +96,39 @@ function innerHeart() {
 const modal = document.querySelector(".modal");
 const addBtn = modal.querySelector(".addBtn");
 
-function addToDo() {
-  modal.classList.toggle("hidden");
+addBtn.addEventListener("click", function () {
+  closeModal();
+  addToDo();
+});
+
+function openModal() {
+  modal.classList.remove("hidden");
 }
 
-addBtn.addEventListener("click", addToDo);
+function closeModal() {
+  modal.classList.add("hidden");
+}
 
-initToDO();
-//해결 못한 것: 하트 안에 숫자 줄이기,,늘리기,,,
-//추가 클릭하고 이거 어케 보여주죠,,아 대충 생각났따,,
-//페이지 이동만들기이 그러면 기본 끝나 히히
+function addToDo() {
+  console.log(clickCategoryId);
+
+  const newToDo = modal.querySelector(".addInput").value;
+  modal.querySelector(".addInput").innerText = newToDo;
+  console.log(newToDo);
+  const clickArticle = document.querySelector(`#${clickCategoryId}`).parentNode;
+
+  const span = document.createElement("span");
+  clickArticle.appendChild(span);
+  span.innerHTML = `
+    ${newToDo}
+    `;
+  span.classList.add("not_done");
+  innerHeart();
+
+  span.addEventListener("click", function () {
+    span.classList.toggle(DONE_CLASSNAME);
+    notDoneNum();
+  });
+
+  console.log(clickArticle.parentNode);
+}
