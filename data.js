0f577107f65ffe1a045868ab5b1aa8583dc51dce
HAE2NI <haein990222@naver.com>
2023-04-21 01:55:10 +0900

    🚚 js 상수 이동

---
+++ data.js
@@ -0,0 +1,76 @@
+const MyMenus = [
+  {
+    name: "아보카도쉬림프샐러드",
+    hashtags: ["생아보카도", "버터새우", "케이준버터"],
+    category: "salad",
+    img: "./assets/saladavocado.jpg",
+  },
+  {
+    name: "치킨텐더샐러드",
+    hashtags: ["닭안심", "머스타드"],
+    category: "salad",
+    img: "./assets/saladchiken.jpg",
+  },
+  {
+    name: "와사비크래미샐러드",
+    hashtags: ["와사비크림", "크래미"],
+    category: "salad",
+    img: "./assets/saladcrap.jpg",
+  },
+  {
+    name: "파스타샐러드",
+    hashtags: ["칠리", "바질", "파스타"],
+    category: "salad",
+    img: "./assets/saladpasta.jpg",
+  },
+  {
+    name: "에그마요샌드위치",
+    hashtags: ["에그마요", "유기농", "달달"],
+    category: "sandwich",
+    img: "./assets/sandeggmayo.jpg",
+  },
+  {
+    name: "햄에그샌드위치",
+    hashtags: ["햄에그", "기본", "체다치즈"],
+    category: "sandwich",
+    img: "./assets/sandhamegg.jpg",
+  },
+  {
+    name: "풀드포크샌드위치",
+    hashtags: ["수비드", "바베큐소스"],
+    category: "sandwich",
+    img: "./assets/sandpolk.jpg",
+  },
+  {
+    name: "단호박에그샌드위치",
+    hashtags: ["단호박", "에그", "건강"],
+    category: "sandwich",
+    img: "./assets/sandpumkin.jpg",
+  },
+  {
+    name: "갈릭감자튀김",
+    hashtags: ["갈릭", "유기농", "올리브오일"],
+    category: "side",
+    img: "./assets/sidecrispypotato.jpg",
+  },
+  {
+    name: "감자수프",
+    hashtags: ["감자", "유기농", "제철"],
+    category: "side",
+    img: "./assets/sidesoup.jpg",
+  },
+  {
+    name: "토마토수프",
+    hashtags: ["토마토", "다이어트", "유기농"],
+    category: "side",
+    img: "./assets/sidetomatosoup.jpg",
+  },
+  {
+    name: "그릭요거트",
+    hashtags: ["꾸덕", "수제", "그래놀라"],
+    category: "side",
+    img: "./assets/sideyogart.jpg",
+  },
+];
+
+export default MyMenus;
--- event.js
+++ event.js
@@ -1,90 +1,17 @@
-const myMenus = [
-  {
-    name: "아보카도쉬림프샐러드",
-    hashtags: ["생아보카도", "버터새우", "케이준버터"],
-    category: "salad",
-    img: "./assets/saladavocado.jpg",
-  },
-  {
-    name: "치킨텐더샐러드",
-    hashtags: ["닭안심", "머스타드"],
-    category: "salad",
-    img: "./assets/saladchiken.jpg",
-  },
-  {
-    name: "와사비크래미샐러드",
-    hashtags: ["와사비크림", "크래미"],
-    category: "salad",
-    img: "./assets/saladcrap.jpg",
-  },
-  {
-    name: "파스타샐러드",
-    hashtags: ["칠리", "바질", "파스타"],
-    category: "salad",
-    img: "./assets/saladpasta.jpg",
-  },
-  {
-    name: "에그마요샌드위치",
-    hashtags: ["에그마요", "유기농", "달달"],
-    category: "sandwich",
-    img: "./assets/sandeggmayo.jpg",
-  },
-  {
-    name: "햄에그샌드위치",
-    hashtags: ["햄에그", "기본", "체다치즈"],
-    category: "sandwich",
-    img: "./assets/sandhamegg.jpg",
-  },
-  {
-    name: "풀드포크샌드위치",
-    hashtags: ["수비드", "바베큐소스"],
-    category: "sandwich",
-    img: "./assets/sandpolk.jpg",
-  },
-  {
-    name: "단호박에그샌드위치",
-    hashtags: ["단호박", "에그", "건강"],
-    category: "sandwich",
-    img: "./assets/sandpumkin.jpg",
-  },
-  {
-    name: "갈릭감자튀김",
-    hashtags: ["갈릭", "유기농", "올리브오일"],
-    category: "side",
-    img: "./assets/sidecrispypotato.jpg",
-  },
-  {
-    name: "감자수프",
-    hashtags: ["감자", "유기농", "제철"],
-    category: "side",
-    img: "./assets/sidesoup.jpg",
-  },
-  {
-    name: "토마토수프",
-    hashtags: ["토마토", "다이어트", "유기농"],
-    category: "side",
-    img: "./assets/sidetomatosoup.jpg",
-  },
-  {
-    name: "그릭요거트",
-    hashtags: ["꾸덕", "수제", "그래놀라"],
-    category: "side",
-    img: "./assets/sideyogart.jpg",
-  },
-];
+import MyMenus from "./data.js";
 const hiddenClass = "hidden";
 
 // 데이터를 카테고리별로 나누기
 function filterCategory(filterdId) {
   switch (filterdId) {
     case "all":
-      return myMenus;
+      return MyMenus;
     case "salad":
-      return myMenus.filter((menu) => menu.category === "salad");
+      return MyMenus.filter((menu) => menu.category === "salad");
     case "sandwich":
-      return myMenus.filter((menu) => menu.category === "sandwich");
+      return MyMenus.filter((menu) => menu.category === "sandwich");
     case "side":
-      return myMenus.filter((menu) => menu.category === "side");
+      return MyMenus.filter((menu) => menu.category === "side");
   }
 }
 
@@ -99,6 +26,13 @@ function isChecked(checkbox) {
     hiddenCard(checkbox.id);
   }
 }
+const checkbox = document.querySelectorAll("input");
+checkbox.forEach((data) => {
+  console.log(data.id);
+  data.addEventListener("change", function (event) {
+    isChecked(event.target);
+  });
+});
 
 // 상위 카테고리 선택된 부분 보여주기
 const all = document.querySelector(".all");
@@ -150,6 +84,13 @@ function xbtnhiddenCard(filterdId) {
   hiddenBtn.classList.add(hiddenClass);
   hiddenCard(filterdId);
 }
+
+const deleteBtn = document.querySelectorAll("button");
+deleteBtn.forEach((data) => {
+  data.addEventListener("click", function (event) {
+    xbtnhiddenCard(event.target);
+  });
+});
 
 //상단 태그에서 엑스버튼 눌렀을 때 체크박스의 체크 사라지기
 function handleCheckbox(checkId) {
