faa5e7de17df3dec162dfa39b5393a2dd031dab1
HAE2NI <haein990222@naver.com>
2023-04-20 12:18:48 +0900

    🎨 저장하는 버튼누르면 localStorage에 추가

--- addCard.js
+++ addCard.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector(".menu").value;
 const tags = document.querySelector(".tag").value;
 const imgs = document.querySelector(".image").value;
+//,로 구분
+const tag = tags.split(",");
 
 //이미지 미리보기
 function loadImg(input) {
@@ -17,3 +19,15 @@ function loadImg(input) {
     preview.src = "";
   }
 }
+
+function saveBtn() {
+  e.preventDefalut();
+
+  //localStorage에 저장
+  localStorage.setItem("menuName", menu);
+  localStorage.setItem("tags", tag);
+  localStorage.setItem("imgs", imgs);
+}
+
+const saveBtn = document.getElementById("saveBtn");
+saveBtn.addEventListener("click", saveBtn());
