5f23f8d044b49830a50709d801636fc745bd1d89
HAE2NI <haein990222@naver.com>
2023-04-20 23:38:18 +0900

    🚚 data 이동

---
+++ todo.js
@@ -0,0 +1,24 @@
+export const toDos = [
+  {
+    category: "할 일!",
+    todos: [
+      { todo: "피아노연습", done: true },
+      { todo: "운동가기", done: true },
+    ],
+  },
+  {
+    category: "SOPT",
+    todos: [
+      { todo: "웹팟 세미나", done: false },
+      { todo: "웹팟 뒤풀이", done: false },
+    ],
+  },
+  {
+    category: "영어 회화",
+    todos: [{ todo: "전화영어", done: true }],
+  },
+  {
+    category: "공부",
+    todos: [{ todo: "인강", done: false }],
+  },
+];
