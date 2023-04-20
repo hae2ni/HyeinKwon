7b3d90eca171a2de9f91cb23742e0e6eb2d40707
HAE2NI <haein990222@naver.com>
2023-04-20 18:44:02 +0900

    🎨 새로운 메뉴 이름, 태그, 카테고리, 이미지 localStorage에 저장

--- addCard.js
+++ addCard.js
@@ -1,8 +1,32 @@
-const menu = document.querySelector(".menu").value;
-const tags = document.querySelector(".tag").value;
-const imgs = document.querySelector(".image").value;
+const tags = document.querySelector(".tag");
+const imgs = document.querySelector(".image");
 //,로 구분
-const tag = tags.split(",");
+
+//새로운 메뉴 이름 입력
+function menuInput() {
+  const menus = document.querySelector("#menuInput").value;
+  localStorage.setItem("newMenu", menus);
+}
+const InputMenu = document.querySelector("#menuInput");
+InputMenu.addEventListener("change", menuInput);
+
+//새로운 태그 입력
+function tagInput() {
+  const tags = document.querySelector("#tagInput").value;
+  const tagsArray = tags.split(",");
+  console.log(tagsArray);
+  localStorage.setItem("newTag", tagsArray);
+}
+const InputTags = document.querySelector("#tagInput");
+InputTags.addEventListener("change", tagInput);
+
+function imgInput() {
+  const img = document.querySelector("#imgInput").files;
+  console.log(img);
+  localStorage.setItem("newImg", img);
+}
+const InputImg = document.querySelector("#imgInput");
+InputImg.addEventListener("change", imgInput);
 
 //이미지 미리보기
 function loadImg(input) {
@@ -20,14 +44,26 @@ function loadImg(input) {
   }
 }
 
-function saveBtn() {
-  e.preventDefalut();
+//카테고리 선택
+function selectCategory() {
+  const category = document.querySelector("#selectCategory");
+  const newCategory = category.value;
+  console.log(newCategory);
+  localStorage.setItem("category", newCategory);
+}
+const CategorySelect = document.querySelector("#selectCategory");
+CategorySelect.addEventListener("change", selectCategory);
 
-  //localStorage에 저장
-  localStorage.setItem("menuName", menu);
-  localStorage.setItem("tags", tag);
-  localStorage.setItem("imgs", imgs);
+function saveIn() {
+  e.preventDefalut();
+  menuInput();
+  tagInput();
+  imgInput();
+  selectCategory();
 }
 
 const saveBtn = document.getElementById("saveBtn");
-saveBtn.addEventListener("click", saveBtn());
+saveBtn.addEventListener("click", function () {
+  location.href = "./salad.html";
+  saveIn();
+});
