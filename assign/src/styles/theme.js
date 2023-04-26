8517c627fc5a5739384e03b176e405605e6e7ec1
HAE2NI <hae0_02ni@ewhain.net>
2023-04-26 12:05:19 +0900

    💄 [STYLE] global style 및 theme 추가

---
+++ assign/src/styles/theme.js
@@ -0,0 +1,13 @@
+const colors = {
+  white: "#FFFFF",
+  black: "#414141",
+  pink: "#FFAEBC",
+  yellow: "#FFF4CB",
+  lightblue: "#AFD6FF",
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
