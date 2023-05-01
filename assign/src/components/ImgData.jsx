fb37a84b315d03f4165faf654dd2db8d45b45de8
HAE2NI <hae0_02ni@ewhain.net>
2023-05-01 22:05:56 +0900

    ♻️ [Refactor] 데이터 구조 변경

--- assign/src/components/ImgData.jsx
+++ assign/src/components/ImgData.jsx
@@ -9,15 +9,42 @@ import imageH from "../assets/image8.gif";
 import imageI from "../assets/image9.gif";
 
 const IMGLIST = [
-  imageA,
-  imageB,
-  imageC,
-  imageD,
-  imageE,
-  imageF,
-  imageG,
-  imageH,
-  imageI,
+  {
+    card: imageA,
+    matched: false,
+  },
+  {
+    card: imageB,
+    matched: false,
+  },
+  {
+    card: imageC,
+    matched: false,
+  },
+  {
+    card: imageD,
+    matched: false,
+  },
+  {
+    card: imageE,
+    matched: false,
+  },
+  {
+    card: imageF,
+    matched: false,
+  },
+  {
+    card: imageG,
+    matched: false,
+  },
+  {
+    card: imageH,
+    matched: false,
+  },
+  {
+    card: imageI,
+    matched: false,
+  },
 ];
 
 //이미지 리스트 섞기
@@ -25,16 +52,17 @@ const RandomImgList = [...IMGLIST].sort(() => Math.random() - 0.5);
 
 // Easy모드 랜덤으로 섞인 리스트에서 5개씩 자르고, 또 랜덤으로 섞어서 붙인 리스트
 const EasyImgList = RandomImgList.slice(0, 5);
-const EasyImgList2 = EasyImgList.sort(() => Math.random() - 0.5);
-
-export const EasyRandomList = EasyImgList.concat(EasyImgList2);
+export const EasyRandomList = [...EasyImgList, ...EasyImgList]
+  .sort(() => Math.random() - 0.5)
+  .map((image) => ({ ...image, id: Math.random() }));
 
 //Normal모드
 const NormalImgList = RandomImgList.slice(0, 7);
-const NormalImgList2 = NormalImgList.sort(() => Math.random() - 0.5);
-
-export const NormalRandomList = NormalImgList.concat(NormalImgList2);
+export const NormalRandomList = [...NormalImgList, ...NormalImgList]
+  .sort(() => Math.random() - 0.5)
+  .map((image) => ({ ...image, id: Math.random() }));
 
 //Hard 모드 (자르는 것 없음), 똑같이 랜덤으로 돌려서 concat으로 붙이기
-const HardImgList = [...IMGLIST].sort(() => Math.random() - 0.5);
-export const HardRandomList = RandomImgList.concat(HardImgList);
+export const HardRandomList = [...IMGLIST, ...IMGLIST]
+  .sort(() => Math.random() - 0.5)
+  .map((image) => ({ ...image, id: Math.random() }));
